c67a6bcb967d7a00634e63a1d2de5735091f24c5
Wei <garfieldlw@gmail.com>
2026-05-17 13:32:06 +0800

    feat: add OIDC discovery and JWKS builder
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ internal/oidc/oidc.go
@@ -0,0 +1,76 @@
+package oidc
+
+import (
+	"crypto"
+	"crypto/rsa"
+	"encoding/base64"
+	"fmt"
+
+	"github.com/go-jose/go-jose/v4"
+
+	"github.com/garfieldlw/OAuth-2.0-and-OpenID-Connect/internal/model"
+)
+
+type JWKSBuilder struct {
+	publicKey *rsa.PublicKey
+	keyID     string
+}
+
+func NewJWKSBuilder(publicKey *rsa.PublicKey, keyID string) *JWKSBuilder {
+	return &JWKSBuilder{
+		publicKey: publicKey,
+		keyID:     keyID,
+	}
+}
+
+func (b *JWKSBuilder) Build() jose.JSONWebKeySet {
+	jwk := jose.JSONWebKey{
+		Key:       b.publicKey,
+		KeyID:     b.keyID,
+		Algorithm: "RS256",
+		Use:       "sig",
+	}
+	return jose.JSONWebKeySet{
+		Keys: []jose.JSONWebKey{jwk},
+	}
+}
+
+func ComputeKeyID(pubKey *rsa.PublicKey) string {
+	jwk := jose.JSONWebKey{
+		Key:       pubKey,
+		Algorithm: "RS256",
+		Use:       "sig",
+	}
+	thumbprint, err := jwk.Thumbprint(crypto.SHA256)
+	if err != nil {
+		return base64.RawURLEncoding.EncodeToString([]byte("fallback-kid"))
+	}
+	return base64.RawURLEncoding.EncodeToString(thumbprint)
+}
+
+type DiscoveryBuilder struct {
+	config *model.AppConfig
+}
+
+func NewDiscoveryBuilder(config *model.AppConfig) *DiscoveryBuilder {
+	return &DiscoveryBuilder{config: config}
+}
+
+func (b *DiscoveryBuilder) Build() *model.OIDCDiscovery {
+	baseURL := b.config.Issuer
+	return &model.OIDCDiscovery{
+		Issuer:                           baseURL,
+		AuthorizationEndpoint:            fmt.Sprintf("%s/oauth/authorize", baseURL),
+		TokenEndpoint:                    fmt.Sprintf("%s/oauth/token", baseURL),
+		UserInfoEndpoint:                 fmt.Sprintf("%s/userinfo", baseURL),
+		JWKSETEndpoint:                   fmt.Sprintf("%s/.well-known/jwks.json", baseURL),
+		ScopesSupported:                  []string{"openid", "profile", "email"},
+		ResponseTypesSupported: []string{"code"},
+		GrantTypesSupported: []string{"authorization_code", "client_credentials", "refresh_token", "password"},
+		SubjectTypesSupported:            []string{"public"},
+		IDTokenSigningAlgValuesSupported: []string{"RS256"},
+		TokenEndpointAuthMethodsSupported: []string{"client_secret_basic", "client_secret_post"},
+		ClaimsSupported:                  []string{"sub", "iss", "aud", "exp", "iat", "nonce", "name", "email"},
+		CodeChallengeMethodsSupported:    []string{"S256", "plain"},
+	}
+}
